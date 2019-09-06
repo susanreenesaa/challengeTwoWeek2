6b427ffddcd027395cb5788b302ee9ba9e1caaec
reenesa <susannins@gmail.com>
2019-09-06 17:21:01 +0300

    declare constants

--- VowelsAndDuplicates.js
+++ VowelsAndDuplicates.js
@@ -1,4 +1,16 @@
 function duplicatesAndVowels(name) {
-  
+  //sorting out vowels in a string
+  const vowels = name.match(/[aeiouAEIOU]/gi);
+  //   removing duplicate vowels from the string
+  const uniqueVowels = [...new Set(vowels)].join("");
+  //   getting duplicates from the original string
+  const numberOfDuplicates = name
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("")
+    .match(/(.)\1+/g).length; //it matches the same subsequent letters
+  var x = [uniqueVowels, numberOfDuplicates];
+  console.log(x);
 }
 duplicatesAndVowels("happy coding at andela");
